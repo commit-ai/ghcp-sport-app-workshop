eb85437419cddbb70e96733a4a927fe9de3dfbeb
Arik Bidny <arikbidny@Ariks-MacBook-Pro-2.local>
2024-10-30 11:45:01 +0200

    fixing issue

--- src/app/(dashboard)/press-conferences/page.tsx
+++ src/app/(dashboard)/press-conferences/page.tsx
@@ -1,24 +1,9 @@
 import ConferenceCard from "@/components/conference-card";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 export default async function PressConferences() {
   const data = await fetch("http://localhost:3001/api/press-conferences");
   const PressConferences = await data.json();
 
-  //   Create an on summarize function that will be called when the button is clicked and will get the transcription of the press conference and summarize it using an AI service
-  //   const onSummarize = () => {
-  //     console.log("Summarize");
-  //   };
-
-  //   console.log(PressConferences);
   return (
     <div className="p-4 h-full">
       <h1 className="text-2xl font-bold">Press Conferences</h1>
